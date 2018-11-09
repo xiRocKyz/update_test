08e78cf6c660698ed7b5aba562d67c05cbef61ba
xiRocKyz <43613410+xiRocKyz@users.noreply.github.com>
2018-11-09 21:18:34 +0300

    Update versions.lua

--- versions.lua
+++ versions.lua
@@ -1,5 +1,5 @@
 local update_testVersions = {--{version = 100, files = {toAdd = {}, toUpdate = {}, toRemove = {}}, description = "example", new = "example"}
-    {version = 100, files = {{}, {}, {}}, description = "First public relase", new = "That's the first version of the update_test library.\nFor more informations please visit\nhttps://forum.mtasa.com/topic/112574-show-update_test-library"},
+    {version = 100, files = {{}, {}, {}}},
 }
 
 function getupdate_testVersions()
