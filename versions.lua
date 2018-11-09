411955ae3974903da7b23eb9f86f26a9841ccb08
xiRocKyz <43613410+xiRocKyz@users.noreply.github.com>
2018-11-09 21:25:02 +0300

    Delete versions.lua

--- versions.lua
+++
@@ -1,8 +0,0 @@
-local update_testVersions = {
-    {version = 100, files = {{}, {}, {}}},
-    {version = 101, files = {{"add.lua", "gg.lua"}, {}, {}}},
-}
-
-function getupdate_testVersions()
-    return update_testVersions
-end
